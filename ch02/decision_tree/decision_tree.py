12af98d59c89175eb663559dbf42f07d481e6301
Ryusei <e1n5te1nrrrrr@gmail.com>
2019-08-19 01:26:52 +0900

    modify decision_tree.py

--- ch02/decision_tree/decision_tree.py
+++ ch02/decision_tree/decision_tree.py
@@ -3,13 +3,19 @@ sys.path.append('../../')
 import mglearn
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.svm import LinearSVC
-from sklearn.datasets import make_blobs
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_breast_cancer
 
 def main():
-    mglearn.plots.plot_tree_progressive()
-    plt.show()
+    cancer = load_breast_cancer()
+    X_train, X_test, y_train, y_test = train_test_split(
+        cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+
+    tree = DecisionTreeClassifier(max_depth=4, random_state=0)
+    tree.fit(X_train, y_train)
+
+    print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+    print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
 
 
 if __name__ == '__main__':
