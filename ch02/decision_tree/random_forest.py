41c12fd9cc2c713c39a70c56e68e54d4d89f7050
Ryusei <e1n5te1nrrrrr@gmail.com>
2019-08-19 23:48:03 +0900

    add random_forest.py

--- ch02/decision_tree/random_forest.py
+++ ch02/decision_tree/random_forest.py
@@ -3,20 +3,21 @@ sys.path.append('../../')
 import mglearn
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+
 
 def main():
     cancer = load_breast_cancer()
     X_train, X_test, y_train, y_test = train_test_split(
         cancer.data, cancer.target, stratify=cancer.target, random_state=42)
 
-    tree = DecisionTreeClassifier(max_depth=4, random_state=0)
-    tree.fit(X_train, y_train)
+    forest = RandomForestClassifier(n_estimators=100, random_state=0)
+    forest.fit(X_train, y_train)
 
-    print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
-    print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+    print("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
+    print("Accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
 
 
 if __name__ == '__main__':
